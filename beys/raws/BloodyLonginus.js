2085664cc39e596290e1dfe0d8aa4b914bf86122
MrRedShadow <redmaurus@hotmail.com>
2021-02-20 20:10:03 -0500

    Bloody fix

--- beys/raws/BloodyLonginus.js
+++ beys/raws/BloodyLonginus.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const bcworkshop = require("bcworkshop");
 const {MessageEmbed} = require("discord.js");
 
@@ -67,7 +66,7 @@ function ReqJoltR(acted, victim, logger){
     //Requirement to activate IF there's a mode change
 }
 function JoltR(acted, victim, logger){
-    acted.atk -= (0.5 + 0.02 * acted.lvl);
+    acted.atk += (0.5 + 0.02 * acted.lvl);
     acted.stamina -= (0.5 - 0.02 * acted.lvl);
     logger.add(`[${acted.username}] Bloody Longinus is on mode **Jolt Rush**!`);
     //What will mode change do
@@ -83,41 +82,5 @@ const BloodyLonginus = new bcworkshop.Beyblade({name:"Bloody Longinus", type: "A
 .attachPassive(DScream4)
 .attachMode(JRush)
 .setDefaultSD("Left");
-=======
-const bcworkshop = new require("bcworkshop");
-
-const passive = new bcworkshop.Passive("Passive", function check(acted, victim, message){
-    return false;
-  }, function passed(acted, victim, message){
-    victim.hp = victim.hp - 28;
-    let embed = new Discord.MessageEmbed()
-    .setTitle(`Uh oh, [${acted.username}] ${acted.bey.bbname || acted.bey.name} tried to use it's passive ability but it was not set up properly. 28 damage dealt.`)
-    .setDescription("Please report this at the support server.")
-    .setColor("#551a8b");
-    message.channel.createMessage({embed: embed});
-  }, 180);
-
-const special = new bcworkshop.Special("Special", function req(acted, victim, logger){return acted.sp > 3}, function special(acted, victim, message){
-    
-    
-    //Change "victim.hp = victim.hp - 123" to "victim.hp = victim.hp - <damage number>. This and the line below can be removed if the special move does not deal any damage.
-    victim.atk = victim.atk = 0;
-    victim.hp = victim.hp - 50;
-    //For more options check the README.md
-    
-    //Make sure to change the "Name", "Special Name" and damage dealt below.
-    let embed = new Discord.MessageEmbed()
-    .setTitle(`[${acted.username}] Bloody Longinus used **Dragon Scream**. 50 damage dealt and opponent's damage nullified for this turn.`)
-    .setColor("#551a8b");
-    
-    message.channel.send(embed);
-  });
-
-const BloodyLonginus = new bcworkshop.Beyblade({name: "Bloody Longinus", type: "Attack", imageLink: "https://vignette.wikia.nocookie.net/beyblade/images/f/f6/BeybladeBloodyLonginusAnime.png/revision/latest?cb=20190918213350"})
-.attachPassive(passive)
-.attachSpecial(special)
-.setDefaultSD("RIGHT")
-.setSDChangable(false);
->>>>>>> upstream/dev
 
 module.exports = BloodyLonginus;
