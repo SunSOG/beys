bdfd1acb109c5451e6f9eee0460a184e65089a89
John Ng <chuchengng@gmail.com>
2021-03-30 10:41:15 +0800

    Added World Spriggan & Za Warudo Spriggan

---
+++ beys/raws/WorldSpriggan.js
@@ -0,0 +1,59 @@
+const bcworkshop = require("bcworkshop");
+
+function wsreq(acted, victim, logger){
+    return acted.hp < (acted.maxhp/100*50) && acted.bey.WSUsed == false;
+}
+
+function wsuse(acted, victim, logger){
+    acted.bey.WSUsed = true;
+    acted.bey.type = "Defense";
+    acted.bey.WorldSpin.active = true;
+    logger.add(`[${acted.username}] **World Spin** activated!`);
+}
+
+const WorldSpin = new bcworkshop.Mode("World Spin", wsreq, wsuse);
+
+function wspreq(acted, victim, logger){
+    return acted.bey.WorldSpin.active == true;
+}
+
+function wspuse(acted, victim, logger){
+    victim.atk = Math.round(victim.atk/100*80);
+    let a=Math.floor(Math.random() * 5);
+    if(a==1)victim.atk=Math.round(victim.atk/100*75);
+}
+
+const WorldSpinPassive = new bcworkshop.Passive("World Spin", wspreq, wspuse);
+
+function sreq(acted, victim, logger){
+    return acted.sp >= 3;
+}
+
+function suse(acted, victim, logger){
+    acted.sp -= 3;
+    victim.hp -= Math.round(20+(1.1*(acted.lvl-1)));
+    logger.add(`[${acted.username}] World Spriggan used **World Slash**!`);
+    acted.stability -= 3;
+}
+
+const WorldSlash = new bcworkshop.Special("World Slash", sreq, suse);
+
+function cbreq(acted, victim, logger){
+    return acted.sp >= 5;
+}
+
+function cbuse(acted, victim, logger){
+    acted.sp -= 5;
+    victim.hp -= Math.round(25+(1.1*(acted.lvl-1)));
+}
+
+const CounterBreak = new bcworkshop.Special("Counter Break", cbreq, cbuse);
+
+const WorldSpriggan = new bcworkshop.Beyblade({name: "World Spriggan", type: "Balance", imageLink: "https://media.discordapp.net/attachments/756736605380935681/826084335492399146/BBSK_World_Spriggan_Unite27_2B_Beyblade.png"})
+.addProperty("WSUsed", false)
+.attachMode(WorldSpin)
+.attachPassive(WorldSpinPassive)
+.attachSpecial(WorldSlash)
+.attachSpecial(CounterBreak);
+
+module.exports = WorldSpriggan;
